428c12f7c5df15133813eae80643956ff7499965
Rafael Ribeiro <54875451+Ycatam@users.noreply.github.com>
2022-01-21 18:39:08 -0300

    Update Main.java

--- TestSolFacil2/Main.java
+++ TestSolFacil2/Main.java
@@ -1,1 +1,21 @@
 
+public class Main {
+ 
+	//code not optimized
+	
+	public static void main(String[] args) {
+
+		String[] games = { "3: 1", "2: 2", "0: 1", "2: 0", "1: 1", "2: 0", "0: 0", "0: 2", "1: 0", "4: 3" };
+
+		String[] par1 = games[0].split("[, ?.@ :]+");
+		String[] par2 = games[1].split("[, ?.@ :]+");
+		String[] par3 = games[2].split("[, ?.@ :]+");
+		String[] par4 = games[3].split("[, ?.@ :]+");
+		String[] par5 = games[4].split("[, ?.@ :]+");
+		
+		int points = 0;
+		int cont = 0;
+		int x, y;
+		
+	}
+}
